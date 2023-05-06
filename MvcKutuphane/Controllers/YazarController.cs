dac2e07a25a35137ebf7c7bd10f6420a734bbec8
batuhan <batuhankeskin@yaani.com>
2023-05-07 01:01:22 +0300

    01:01 7.05.23

--- MvcKutuphane/Controllers/YazarController.cs
+++ MvcKutuphane/Controllers/YazarController.cs
@@ -18,7 +18,7 @@ namespace MvcKutuphane.Controllers
         }
         [HttpGet]
         public ActionResult YazarEkle()
-        { 
+        {
             return View();
         }
         [HttpPost]
@@ -33,6 +33,21 @@ namespace MvcKutuphane.Controllers
         {
             var yazar = db.TBLYAZAR.Find(id);
             db.TBLYAZAR.Remove(yazar);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult YazarGetir(int id)
+        {
+            var yzr = db.TBLYAZAR.Find(id);
+
+            return View("YazarGetir", yzr);
+        }
+        public ActionResult YazarGuncelle(TBLYAZAR p)
+        {
+            var yzr = db.TBLYAZAR.Find(p.ID);
+            yzr.AD = p.AD;
+            yzr.SOYAD = p.SOYAD;
+            yzr.DETAY = p.DETAY;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
