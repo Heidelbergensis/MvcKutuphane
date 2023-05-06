ddaa47e9dbcd669c88c1d90eeca8ada568f3d7de
batuhan <batuhankeskin@yaani.com>
2023-05-07 02:02:27 +0300

    02:02 7.05.23

--- MvcKutuphane/Controllers/KitapController.cs
+++ MvcKutuphane/Controllers/KitapController.cs
@@ -1,17 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.Mvc;
-
+using MvcKutuphane.Models.Entity;
 namespace MvcKutuphane.Controllers
 {
     public class KitapController : Controller
     {
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
         // GET: Kitap
         public ActionResult Index()
         {
+            var kitaplar = db.TBLKITAP.ToList();
+            return View(kitaplar);
+        }
+        [HttpGet]
+        public ActionResult KitapEkle()
+        {
+            List<SelectListItem> deger1 = (from i in db.TBLKATEGORI.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = i.ADI,
+                                               Value = i.ID.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            List<SelectListItem> deger2 = (from i in db.TBLYAZAR.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = i.AD + ' ' + i.SOYAD,
+                                               Value = i.ID.ToString()
+                                           }).ToList();
+            ViewBag.dgr2 = deger2;
             return View();
         }
+        [HttpPost]
+        public ActionResult KitapEkle(TBLKITAP p)
+        {
+            var ktg = db.TBLKATEGORI.Where(k => k.ID == p.TBLKATEGORI.ID).FirstOrDefault();
+            var yzr = db.TBLYAZAR.Where(y => y.ID == p.TBLYAZAR.ID).FirstOrDefault();
+            p.TBLKATEGORI = ktg;
+            p.TBLYAZAR = yzr;
+            db.TBLKITAP.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult KitapSil(int id)
+        {
+            var kitap = db.TBLKITAP.Find(id);
+            db.TBLKITAP.Remove(kitap);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult KitapGetir(int id)
+        {
+            var ktp = db.TBLKITAP.Find(id);
+
+            return View("KitapGetir", ktp);
+        }
+        //public ActionResult YazarGuncelle(TBLYAZAR p)
+        //{
+        //    var yzr = db.TBLYAZAR.Find(p.ID);
+        //    yzr.AD = p.AD;
+        //    yzr.SOYAD = p.SOYAD;
+        //    yzr.DETAY = p.DETAY;
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
     }
 }
